7a65fee492511999f2797a4292b71b222db5b224
Pedja <vpedja12@gmail.com>
2019-11-23 12:43:47 -0800

    reservation and waitlist functionality

--- server.js
+++ server.js
@@ -12,15 +12,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const reservations = [
-    {
-     
-    }];
+const reservations = [];
 
-const waitList = [
-    {
-    
-}];
+const waitList = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
@@ -43,21 +37,22 @@ app.get("/api/waitlist", function (req, res) {
     return res.json(waitList);
 });
 
-app.post("/api/reservations", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
+app.post("/api/reservations", function (req, res) {
     var newReservation = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+
     newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
+
     console.log(newReservation);
-  
-    reservations.push(newReservation);
-  
+
+    if (reservations.length === 5) {
+        waitList.push(newReservation);
+
+    } else {
+        reservations.push(newReservation);
+    }
+
     res.json(newReservation);
-  });
+});
 
 
 app.listen(PORT, function () {
