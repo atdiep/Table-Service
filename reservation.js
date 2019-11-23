87c1e7f6a0852e5de25fe44d000fbbcdda2e7b72
CJ Conrad <carlj.conrad@CJs-MacBook-Pro.local>
2019-11-23 12:50:15 -0800

    updating master

--- reservation.js
+++ reservation.js
@@ -7,3 +7,28 @@ class Reservations {
     this.id = id,
     }
 }
+
+// user post table res to api
+// 1. .get to api/reservations to get reservations
+function getReservations() {
+    axios.get("localhost:3000/api/reservations")
+        .then((resArray) => {
+            buildResList(resArray);
+        })
+}
+// 2. create reservation list
+function buildResList(resArray) {
+    var stringArray = [];
+    res.forEach(reservation => {
+       const reservation = createHTML(resObj);
+       stringArray.push(reservation)
+        }
+
+    })
+}
+
+function createHTML(resObj) {
+    return ``
+    }
+
+}
